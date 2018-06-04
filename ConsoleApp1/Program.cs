1ae0547db4edcca9b696bb3ca07dcaeb0748cccb
ichbineinNerd <34607504+ichbineinNerd@users.noreply.github.com>
2018-06-04 17:56:55 +0200

    added print command

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,10 +16,10 @@ namespace ConsoleApp1
 
 		static string[] splitbycommands;
 
-		Dictionary<string, int> Variables = new Dictionary<string, int>();//maybe at some point there'll be support for something else than ints.
-		
+		static Dictionary<string, int> Variables = new Dictionary<string, int>();//maybe at some point there'll be support for something else than ints.
+
 		static bool isDone = false;
-		
+
 		static String input;
 
 		public static void Main(String[] args)
@@ -30,7 +30,7 @@ namespace ConsoleApp1
 				{
 					input = File.ReadAllText(args[0]);
 
-					splitbycommands = input.Split(';');	
+					splitbycommands = input.Split(';');
 
 					GetDeeperMeaning();
 
@@ -49,56 +49,95 @@ namespace ConsoleApp1
 			}
 		}
 
-		public static void GetDeeperMeaning	()
+		public static void GetDeeperMeaning()
 		{
 			//return 42;
-			
+
 			foreach (string command in splitbycommands)
 			{
-				string[] words = command.Split(null);
-
-				if (words[words.Length - 1].ToLower() != "now") {
-					Thread t = new Thread(scheduletask(command + " now"));
-					continue;
-				}
-				
-				if (words[0].ToLower().Equals("set"))
-				{
-					foreach (string word in reservedwords) {
-						
-						if (words[1].ToLower().Equals(word))
-							throw new ReservedException("The variable you're trying to assign a value to is a reserved word. (" + word + ")");
-					
-					}
-
-					int val;
-					
-					if (!int.TryParse(words[3].ToLower(), out val)) 
-					{
-						throw new NaNException("\"" + words[3].ToLower() + "\" is not a number.");
-					}
-					
-					if (Variables.ContainsKey(words[1])) {
-						Variables[words[1]] = val;
-					}else {
-						Variables.Add(words[1], val);
-					}
-				}
+				executecmd(command);
 			}
-			
+
 			isDone = true;
 		}
 
 		public static void scheduletask(string cmd) {
 			if (isDone) {
-				
-			}else {
+				executecmd(cmd);
+			} else {
 				Thread.Sleep(100);
 				scheduletask(cmd);
 			}
 		}
 
-		public static void printarrays(object[] array)
+		public static void executecmd(string command)
+		{
+			string[] words = command.Split(null);
+
+			if (words[words.Length - 1].ToLower() != "now")
+			{
+				Thread t = new Thread(() => scheduletask(command + " now"));
+				return;
+			}
+
+			if (words[0].ToLower().Equals("set"))
+			{
+				foreach (string word in reservedwords)
+				{
+
+					if (words[1].ToLower().Equals(word))
+						throw new ReservedException("The variable you're trying to assign a value to is a reserved word. (" + word + ")");
+
+				}
+					 
+				if (!int.TryParse(words[3].ToLower(), out int val))
+				{
+					throw new NaNException("\"" + words[3].ToLower() + "\" is not a number.");
+				}
+
+				if (!words[2].ToLower().Equals("to") && !words[2].ToLower().Equals("="))
+				{
+					throw new Exception("The third word when setting a variable should be \"=\" or \"to\"");
+				}
+
+				if (int.TryParse(words[1], out int stuff))
+				{
+					throw new Exception("Your Variable Name can't be numeric");
+				}
+
+				if (Variables.ContainsKey(words[1]))
+				{
+					Variables[words[1]] = val;
+				}
+				else
+				{
+					Variables.Add(words[1], val);
+				}
+			}
+			else if (words[0].ToLower().Equals("print"))
+			{	
+				Console.Write(words[1]);
+				if (!words[2].ToLower().Equals("and"))
+				{
+					if (!words[3].ToLower().Equals("nothing"))
+					{
+						if (!words[4].ToLower().Equals("else"))
+						{
+							string gibberish = "";
+
+							for (int i = 0; i < words[1].Length; i++)
+							{
+								gibberish += (char)new Random().Next(128);
+							}
+
+							Console.Write(gibberish);
+						}
+					}
+				}
+			}
+		}
+
+		public static void Printarrays(object[] array)
 		{
 			foreach(object o in array)
 			{
